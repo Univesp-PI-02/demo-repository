2bf857f5d4ed30c9ea83dc1b1771e5257f405c33
Thais Faustino <thaisfsouza@outlook.com>
2024-09-22 15:49:33 -0300

    inclui get na rota clientes

--- projeto_cad/app_cad_clientes/views.py
+++ projeto_cad/app_cad_clientes/views.py
@@ -9,25 +9,25 @@ def cadastro(request):
     return render(request,'clientes/cadastro.html')
 
 def clientes(request):
-    # Salvar
-    novo_cliente = Clientes(
-        empresa=request.POST.get('empresa'),
-        segmento=request.POST.get('segmento'),
-        contato=request.POST.get('contato'),
-        telefone=request.POST.get('telefone'),
-        cidade=request.POST.get('cidade'),
-        estado=request.POST.get('estado'),
-        email=request.POST.get('email'),
-        instagram=request.POST.get('instagram'),
-        cnpj=request.POST.get('cnpj'),
-        qtd_lojas=request.POST.get('qtd_lojas'),
-        observacoes=request.POST.get('observacoes'),
-        prox_contato=request.POST.get('prox_contato'))
-    novo_cliente.save()
-    
-    # Exibir todos os clientes
+    if request.method == 'POST':
+        # Salvar novo cliente se o método for POST
+        novo_cliente = Clientes(
+            empresa=request.POST.get('empresa'),
+            segmento=request.POST.get('segmento'),
+            contato=request.POST.get('contato'),
+            telefone=request.POST.get('telefone'),
+            cidade=request.POST.get('cidade'),
+            estado=request.POST.get('estado'),
+            email=request.POST.get('email'),
+            instagram=request.POST.get('instagram'),
+            cnpj=request.POST.get('cnpj'),
+            qtd_lojas=request.POST.get('qtd_lojas'),
+            observacoes=request.POST.get('observacoes'),
+            prox_contato=request.POST.get('prox_contato')
+        )
+        novo_cliente.save()
+
     clientes = {
         'clientes': Clientes.objects.all()
     }
-    # Retornar clientes para a pag de listagem
-    return render(request,'clientes/busca.html',clientes)
+    return render(request, 'clientes/busca.html', clientes)
