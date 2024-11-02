c55e69fedb0f9e007c089d665e598a5adbf1a5b6
Thais Faustino <thaisfsouza@outlook.com>
2024-11-02 14:23:48 -0300

    troca allowed_hosts

--- projeto_cad/projeto_cad/settings.py
+++ projeto_cad/projeto_cad/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gs1!vmql@7$nc7u^b+&&g9#u2$iimni8(37@ssxx_0h#jbler$"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
