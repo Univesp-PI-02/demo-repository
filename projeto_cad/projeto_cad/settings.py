42aaef42ac51816f6713ac82a0cdaa852868138c
Thais Faustino <thaisfsouza@outlook.com>
2024-11-02 18:17:15 -0300

    configs render ok

--- projeto_cad/projeto_cad/settings.py
+++ projeto_cad/projeto_cad/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = ["https://projeto-cad-test.onrender.com"]
+
+port = os.getenv("PORT", "8000")
+
 
 # Application definition
 
@@ -77,9 +81,13 @@ WSGI_APPLICATION = "projeto_cad.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
